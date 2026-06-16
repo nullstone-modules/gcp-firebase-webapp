86642f88c62c31ec6faec61cc6067d22420a07f2
Brad Sickles <brad.sickles@gmail.com>
2026-06-15 20:58:20 -0400

    Dropped default api key.

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,3 @@ output "web_app_name" {
   value       = google_firebase_web_app.this.name
   description = "The fully qualified resource name of the App. Format: projects/{{project}}/webApps/{{appId}}"
 }
-
-output "api_key_id" {
-  value       = google_apikeys_key.web.key_string
-  description = "string ||| The Firebase Web API Key used to embed in client-side code that identify the project."
-  sensitive   = true
-}
